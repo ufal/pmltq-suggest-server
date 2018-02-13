0d1017fed7ff810298d995b44ae7e12d2a8ac5a7
Matyáš Kopp <matyas.kopp@gmail.com>
2018-02-13 16:12:52 +0100

    suggest command for pmltq script

--- lib/PMLTQ/Command/suggest.pm
+++ lib/PMLTQ/Command/suggest.pm
@@ -3,15 +3,40 @@ package PMLTQ::Command::suggest;
 # ABSTRACT: Return query for given nodes
 
 use PMLTQ::Base 'PMLTQ::Command';
+use PMLTQ;
+use PMLTQ::Suggest::Utils;
+use PMLTQ::Suggest;
 use File::Path qw( make_path );
+use Getopt::Long qw(GetOptionsFromArray);
+use Treex::PML;
 
 has usage => sub { shift->extract_usage };
 
+my %opts;
+
 sub run {
   my $self = shift;
-
-  print STDERR "TODO: PMLTQ::Command::suggest\n";
-
+  my @args = @_;
+  GetOptionsFromArray(\@args, \%opts,
+    'nodes|N=s'
+  )  || die "invalid options";
+  Treex::PML::AddResourcePath(
+       PMLTQ->resources_dir,
+       File::Spec->catfile(${FindBin::RealBin},'config'),
+       $ENV{HOME}.'/.tred.d',
+       $self->config->{resources}
+      );
+  my @paths = $opts{nodes} ? split(/\|/, $opts{nodes}) : ();
+  my @positions;
+  foreach my $p (@paths) {
+    my ($path, $goto)=PMLTQ::Suggest::Utils::parse_file_suffix($p);
+    $path = URI->new($path)->canonical->as_string;
+    push @positions, [$path,$goto];
+  }
+  my $pmltq = PMLTQ::Suggest::make_pmltq(
+    \@positions
+   );
+  print "$pmltq\n";
 }
 
 =head1 SYNOPSIS
